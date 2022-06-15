6bbbd8ed28c57e35338772fac158856e05eecdf6
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2022-06-15 11:03:37 -0700

    Add command-invoking custom control written in VB.

---
+++ src/WinFormsCommandBinding - ModifiedRuntime/CommandTrackBar/CommandTrackBar.vb
@@ -0,0 +1,94 @@
+﻿Imports System.ComponentModel
+Imports System.Windows.Input
+
+Public Class CommandTrackBar
+    Inherits TrackBar
+    Implements ICommandProvider
+
+    Private _command As ICommand
+    Private _commandParameter As Object
+
+    Public Event CommandChanged As EventHandler Implements ICommandProvider.CommandChanged
+    Public Event CommandCanExecuteChanged As EventHandler Implements ICommandProvider.CommandCanExecuteChanged
+    Public Event CommandExecute As CancelEventHandler Implements ICommandProvider.CommandExecute
+    Public Event CommandParameterChanged As EventHandler
+
+    Public Property Command As ICommand _
+        Implements ICommandProvider.Command
+        Get
+            Return _command
+        End Get
+        Set(value As ICommand)
+            ICommandProvider.CommandSetter(Me, value, _command)
+        End Set
+    End Property
+
+    Public Property CommandParameter As Object _
+        Implements ICommandProvider.CommandParameter
+        Get
+            Return _commandParameter
+        End Get
+        Set(value As Object)
+            _commandParameter = value
+            OnCommandParameterChanged(EventArgs.Empty)
+        End Set
+    End Property
+
+    Protected Overridable Sub OnCommandParameterChanged(e As EventArgs)
+        RaiseEvent CommandParameterChanged(Me, e)
+    End Sub
+
+    Protected Overrides Sub OnDoubleClick(e As EventArgs)
+        MyBase.OnDoubleClick(e)
+        ICommandProvider.RequestCommandExecute(Me)
+    End Sub
+
+    ''' <summary>
+    ''' Provides the previous Enabled value for the ICommandProvider implementation.
+    ''' </summary>
+    ''' <remarks>
+    ''' The interface uses DFI to bring a consistent Command binding implementation along.
+    ''' It therefore needs a backing field to save a previous Enabled setting, since
+    ''' a components/controls Enabled property Is automatically controlled by the Command
+    ''' binding through the CanExecute functionality of the Command binding.
+    ''' </remarks>
+    Private Property PreviousEnabledStatus As Boolean? _
+        Implements ICommandProvider.PreviousEnabledStatus
+
+    Private Shadows Property ICommandProvider_Enabled As Boolean _
+        Implements ICommandProvider.Enabled
+        Get
+            Return MyBase.Enabled
+        End Get
+        Set(value As Boolean)
+            MyBase.Enabled = value
+        End Set
+    End Property
+
+    Private Sub HandleCommandChanged(e As EventArgs) _
+            Implements ICommandProvider.HandleCommandChanged
+        OnCommandChanged(e)
+    End Sub
+
+    Private Sub HandleCommandCanExecuteChanged(sender As Object, e As EventArgs) _
+        Implements ICommandProvider.HandleCommandCanExecuteChanged
+        OnCommandCanExecuteChanged(sender, e)
+    End Sub
+
+    Private Sub HandleCommandExecute(e As CancelEventArgs) _
+        Implements ICommandProvider.HandleCommandExecute
+        OnCommandExecute(e)
+    End Sub
+
+    Protected Overridable Sub OnCommandChanged(e As EventArgs)
+        RaiseEvent CommandChanged(Me, e)
+    End Sub
+
+    Protected Overridable Sub OnCommandCanExecuteChanged(sender As Object, e As EventArgs)
+        RaiseEvent CommandCanExecuteChanged(sender, e)
+    End Sub
+
+    Protected Overridable Sub OnCommandExecute(e As EventArgs)
+        RaiseEvent CommandExecute(Me, e)
+    End Sub
+End Class
--- src/WinFormsCommandBinding - ModifiedRuntime/WinFormsCommandBindingDemo/OptionsForm.Designer.cs
+++ src/WinFormsCommandBinding - ModifiedRuntime/WinFormsCommandBindingDemo/OptionsForm.Designer.cs
@@ -46,12 +46,13 @@ namespace WinFormsCommandBindingDemo
             // 
             // _okButton
             // 
+            this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this._okButton.DataBindings.Add(new System.Windows.Forms.Binding("Command", this._optionsFormControllerBindingSource, "OKCommand", true));
-            this._okButton.DataContext = null;
             this._okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this._okButton.Location = new System.Drawing.Point(233, 221);
+            this._okButton.Location = new System.Drawing.Point(406, 353);
+            this._okButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this._okButton.Name = "_okButton";
-            this._okButton.Size = new System.Drawing.Size(113, 28);
+            this._okButton.Size = new System.Drawing.Size(184, 45);
             this._okButton.TabIndex = 7;
             this._okButton.Text = "OK";
             this._okButton.UseVisualStyleBackColor = true;
@@ -62,11 +63,12 @@ namespace WinFormsCommandBindingDemo
             // 
             // commandButton1
             // 
-            this.commandButton1.DataContext = null;
+            this.commandButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.commandButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.commandButton1.Location = new System.Drawing.Point(352, 221);
+            this.commandButton1.Location = new System.Drawing.Point(599, 353);
+            this.commandButton1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.commandButton1.Name = "commandButton1";
-            this.commandButton1.Size = new System.Drawing.Size(113, 28);
+            this.commandButton1.Size = new System.Drawing.Size(184, 45);
             this.commandButton1.TabIndex = 8;
             this.commandButton1.Text = "Cancel";
             this.commandButton1.UseVisualStyleBackColor = true;
@@ -74,10 +76,10 @@ namespace WinFormsCommandBindingDemo
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.DataContext = null;
-            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Location = new System.Drawing.Point(41, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.Size = new System.Drawing.Size(143, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Text Option:";
             // 
@@ -85,66 +87,72 @@ namespace WinFormsCommandBindingDemo
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this._optionsFormControllerBindingSource, "BoolOption", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.checkBox1.DataContext = null;
-            this.checkBox1.Location = new System.Drawing.Point(181, 60);
+            this.checkBox1.Location = new System.Drawing.Point(294, 96);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(145, 24);
+            this.checkBox1.Size = new System.Drawing.Size(231, 36);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "CheckBox Option";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this._optionsFormControllerBindingSource, "TextOption", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.textBox1.DataContext = null;
-            this.textBox1.Location = new System.Drawing.Point(181, 12);
+            this.textBox1.Location = new System.Drawing.Point(294, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(284, 27);
+            this.textBox1.Size = new System.Drawing.Size(486, 39);
             this.textBox1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.DataContext = null;
-            this.label2.Location = new System.Drawing.Point(25, 108);
+            this.label2.Location = new System.Drawing.Point(41, 173);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.Size = new System.Drawing.Size(191, 32);
             this.label2.TabIndex = 3;
             this.label2.Text = "Numeric Option:";
             // 
             // numericUpDown1
             // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.numericUpDown1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this._optionsFormControllerBindingSource, "NumOption", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.numericUpDown1.DataContext = null;
-            this.numericUpDown1.Location = new System.Drawing.Point(181, 106);
+            this.numericUpDown1.Location = new System.Drawing.Point(294, 170);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(284, 27);
+            this.numericUpDown1.Size = new System.Drawing.Size(489, 39);
             this.numericUpDown1.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.DataContext = null;
-            this.label3.Location = new System.Drawing.Point(25, 158);
+            this.label3.Location = new System.Drawing.Point(41, 253);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.Size = new System.Drawing.Size(191, 32);
             this.label3.TabIndex = 5;
             this.label3.Text = "Numeric Option:";
             // 
             // dateTimePicker1
             // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePicker1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this._optionsFormControllerBindingSource, "NumOption", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.dateTimePicker1.DataContext = null;
-            this.dateTimePicker1.Location = new System.Drawing.Point(181, 153);
+            this.dateTimePicker1.Location = new System.Drawing.Point(294, 245);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(284, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(486, 39);
             this.dateTimePicker1.TabIndex = 6;
             // 
             // OptionsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 257);
+            this.ClientSize = new System.Drawing.Size(804, 416);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown1);
@@ -155,6 +163,7 @@ namespace WinFormsCommandBindingDemo
             this.Controls.Add(this.commandButton1);
             this.Controls.Add(this._okButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "OptionsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "OptionsForm";
