30bc516cf6a93a59bcf9f2d66ff4ee61b4db3fb2
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2022-06-17 21:11:27 -0700

    Introduce patch for missing Android CursorPosition functionality.

---
+++ src/Maui/MauiEdit/MauiEdit/MainPage.xaml.PlatformSpecific.cs
@@ -0,0 +1,52 @@
+﻿#if ANDROID
+	using AndroidX.AppCompat.Widget;
+#endif
+
+#if WINDOWS
+	using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace MauiEdit;
+
+public partial class MainPage : ContentPage
+{
+#if ANDROID
+    private Timer _timer;
+#endif
+
+	partial void InitializePlatformDependentComponent()
+	{
+        _editor.HandlerChanged += Editor_HandlerChanged;
+#if ANDROID
+        _timer = new Timer(TimerCallback, null, 0, 200);
+#endif
+	}
+
+#if ANDROID
+    private void TimerCallback(object state)
+	{
+		if (_editor.Handler?.PlatformView is AppCompatEditText editor)
+		{
+			_editor.CursorPosition=editor.SelectionStart;
+			_editor.SelectionLength = editor.SelectionEnd - editor.SelectionStart;
+		}
+    }
+#endif
+
+    private void Editor_HandlerChanged(object sender, EventArgs e)
+	{
+		var editor=(Editor)sender;
+		var editorHandler = editor.Handler;
+
+#if WINDOWS
+		TextBox platformView = (TextBox) editorHandler.PlatformView;
+
+		platformView.SelectionChanged += (sender, eArgs)=>
+		{
+			var textBox = (Microsoft.UI.Xaml.Controls.TextBox)sender;
+			_editor.CursorPosition = textBox.SelectionStart;
+			_editor.SelectionLength = textBox.SelectionLength;
+        };
+#endif
+	}
+}
--- src/Maui/MauiEdit/MauiEdit/MainPage.xaml.cs
+++ src/Maui/MauiEdit/MauiEdit/MainPage.xaml.cs
@@ -1,30 +1,12 @@
-﻿using System.Diagnostics;
-
-namespace MauiEdit;
+﻿namespace MauiEdit;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-		_editor.HandlerChanged += Editor_HandlerChanged; ;
+		InitializePlatformDependentComponent();
 	}
 
-	private void Editor_HandlerChanged(object sender, EventArgs e)
-	{
-		var editor=(Editor)sender;
-		var editorHandler = editor.Handler;
-
-#if WINDOWS
-		Microsoft.UI.Xaml.Controls.TextBox platformView = 
-			(Microsoft.UI.Xaml.Controls.TextBox) editorHandler.PlatformView;
-
-		platformView.SelectionChanged += (sender, eArgs)=>
-		{
-			var textBox = (Microsoft.UI.Xaml.Controls.TextBox)sender;
-			_editor.CursorPosition = textBox.SelectionStart;
-			_editor.SelectionLength = textBox.SelectionLength;
-        };
-#endif
-    }
+	partial void InitializePlatformDependentComponent();
 }
